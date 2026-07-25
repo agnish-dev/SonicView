07faf3e9a5d65d5b50ccbb606f3875a625279b70
agnish-dev <hernberd628@gmail.com>
2026-07-25 13:06:18 +0530

    pusing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rkvlgk0oqfo"
+    "revision": "0.njtskju2in"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
